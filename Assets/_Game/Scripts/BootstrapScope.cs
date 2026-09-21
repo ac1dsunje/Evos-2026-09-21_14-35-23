dfdaa24d8c424e66f7e5c4f4d12033a55805ec69
ac1dsunje <ar.cherba63programming@gmail.com>
2026-09-21 15:00:20 +0200

    initial setup

---
+++ Assets/_Game/Scripts/BootstrapScope.cs
@@ -0,0 +1,13 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace _Game.Scripts
+{
+public class BootstrapScope : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+        
+    }
+}
+}
