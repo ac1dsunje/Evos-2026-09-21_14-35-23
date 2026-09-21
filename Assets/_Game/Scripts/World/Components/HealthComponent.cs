87ea039cb20ecc0270066a0de8edc17b6fe13daa
ac1dsunje <ar.cherba63programming@gmail.com>
2026-09-21 16:17:25 +0200

    fix

--- Assets/_Game/Scripts/World/Components/HealthComponent.cs
+++ Assets/_Game/Scripts/World/Components/HealthComponent.cs
@@ -1,8 +1,10 @@
-﻿using FFS.Libraries.StaticEcs;
+﻿using System;
+using FFS.Libraries.StaticEcs;
 using FFS.Libraries.StaticEcs.Unity;
 
 namespace _Game.Scripts.World.Components
 {
+[Serializable]
 public struct HealthComponent : IComponent
 {
     [StaticEcsEditorTableValue] public float Value;
--- Assets/_Game/Scripts/World/Components/NameComponent.cs
+++ Assets/_Game/Scripts/World/Components/NameComponent.cs
@@ -1,8 +1,10 @@
-﻿using FFS.Libraries.StaticEcs;
+﻿using System;
+using FFS.Libraries.StaticEcs;
 using FFS.Libraries.StaticEcs.Unity;
 
 namespace _Game.Scripts.World.Components
 {
+[Serializable]
 public struct NameComponent : IComponent
 {
     [StaticEcsEditorTableValue] public string Name;
